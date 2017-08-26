b627c18f0e33d1f8da653c1de91912215cd69b79
TheNouillet <axel.veber@gmail.com>
2017-08-26 12:53:43 +0200

    refactoring

--- bot.js
+++ bot.js
@@ -1,19 +1,18 @@
 const Discord = require('discord.js');
-var inspect = require('eyes').inspector({ maxLength: false });
-var parser = require("./parser");
+
 const AudioListener = require("./lib/Listener/AudioListener");
 const HelpListener = require("./lib/Listener/HelpListener");
+const ThemeService = require('./lib/Utils/ThemeService');
 
 const client = new Discord.Client();
 var botToken = process.argv[2];
-var specialCharacter = '!';
-var fileMap = parser.get();
+var fileMap = new ThemeService().parseCommandFile(__dirname + '/cmds.xml');
 
 client.canBroadcast = true;
 client.on('ready', () => {
     client.messageListeners = [];
-    client.messageListeners.push(new AudioListener(fileMap, specialCharacter));
-    client.messageListeners.push(new HelpListener(fileMap, specialCharacter));
+    client.messageListeners.push(new AudioListener(fileMap));
+    client.messageListeners.push(new HelpListener(fileMap));
     console.log('Ready!');
 });
 client.on('message', message => {
